65a8415f38f9791d58ddda88a2d4d239c87005de
student <student@234-1>
2016-11-25 13:28:19 +0100

    Application Startup (app class)

---
+++ JHotDraw5.1/sources/PSystemStart/PlanetSystem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PSystemStart;
+
+import CH.ifa.draw.application.*;
+
+/**
+ *
+ * @author student
+ */
+public class PlanetSystem extends DrawApplication {
+     PlanetSystem(){
+         super("Planet System");
+     }
+     
+     public static void main(String[] args){
+         PlanetSystem ps = new PlanetSystem();
+         ps.open();
+         
+     
+     }
+    
+}
