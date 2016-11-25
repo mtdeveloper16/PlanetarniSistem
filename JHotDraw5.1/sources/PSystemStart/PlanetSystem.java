72c831751c8e0c7b7602b1903b3213d189db68b3
student <student@234-1>
2016-11-25 14:57:50 +0100

    Last:: SouthEastLocatorHandle

--- JHotDraw5.1/sources/PSystemStart/PlanetSystem.java
+++ JHotDraw5.1/sources/PSystemStart/PlanetSystem.java
@@ -6,6 +6,15 @@ package PSystemStart;
 
 import CH.ifa.draw.application.*;
 
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+import CH.ifa.draw.framework.*;
+import CH.ifa.draw.standard.*;
+import CH.ifa.draw.figures.*;
+import CH.ifa.draw.util.*;
+import PSystemStart.Planet;
+
 /**
  *
  * @author student
@@ -15,11 +24,26 @@ public class PlanetSystem extends DrawApplication {
          super("Planet System");
      }
      
+     
+     @Override
+     public void createTools(Panel palette){
+         super.createTools(palette);
+         
+         CreationTool ellipseTool = new CreationTool(view(), new EllipseFigure() );
+         ToolButton ellipseButton = new ToolButton(this, IMAGES+"ELLIPSE", "Ellipse Tool", ellipseTool);
+         palette.add(ellipseButton);
+         
+         CreationTool planetTool = new CreationTool(view(), new Planet());
+         ToolButton planetButton = new ToolButton(this,IMAGES+"DIAMOND", "Planet", planetTool);
+         palette.add(planetButton);
+     
+     }
+     
      public static void main(String[] args){
          PlanetSystem ps = new PlanetSystem();
          ps.open();
          
-     
+         
      }
     
 }
